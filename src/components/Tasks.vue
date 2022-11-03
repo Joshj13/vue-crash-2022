dfc98d0b68af18be053b0091d5ef04516cdc779c
josh jacobs <joshuajacobs87@gmail.com>
2022-11-03 22:00:05 +0200

    styling added

--- src/components/Tasks.vue
+++ src/components/Tasks.vue
@@ -1,6 +1,6 @@
 <template>
     <div :key="task.id" v-for="task in tasks">
-        <h3>{{task.text}}</h3>
+      <Task :task="task"/>
     </div>
     
 </template>
@@ -12,6 +12,9 @@
         name: 'Tasks',
         props: {
             tasks:Array,
+        },
+        components: {
+            Task,
         }
     }
 </script>  
